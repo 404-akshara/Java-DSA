935b1006cdb18b6661c72ce50f8c88655ad5d757
Akshara Jain <aksharaj170@gmail.com>
2025-09-07 20:40:16 +0530

    Added program for prime number
    this program takes any number as input from user and prints if it is a prime number or not.
    
    Signed-off-by: Akshara Jain <aksharaj170@gmail.com>

---
+++ loops/Prime_number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Prime_number {
+    public static void main(String[]args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int count=0;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count>0)
+        {
+            System.out.println("it is not a prime number.");
+        }
+        else{
+            System.out.println("it is a prime number");
+        }
+    }
+}
