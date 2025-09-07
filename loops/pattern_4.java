2873fe53a85fc13261b7d64f28e70095027fc54e
Akshara Jain <aksharaj170@gmail.com>
2025-09-07 21:09:16 +0530

    added program for pattern_4
    Signed-off-by: Akshara Jain <aksharaj170@gmail.com>

---
+++ loops/pattern_4.java
@@ -0,0 +1,15 @@
+public class pattern_4
+{
+    public static void main(String[]args)
+    {
+    for(int i=1;i<=4;i++)
+    {
+      for(int j=4;i<=j;j--)
+      {
+        System.out.print("*");
+        
+      }
+      System.out.println();
+    }
+}
+}
