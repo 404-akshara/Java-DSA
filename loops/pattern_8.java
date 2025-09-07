1f6908638539b01d77c610fcd0d3deda49afea42
Akshara Jain <aksharaj170@gmail.com>
2025-09-07 22:57:01 +0530

    added the program for pattern-8
    Signed-off-by: Akshara Jain <aksharaj170@gmail.com>

---
+++ loops/pattern_8.java
@@ -0,0 +1,15 @@
+public class pattern_8
+{
+    public static void main(String[]args)
+    { 
+    for(int i=1;i<=5;i++)
+    { int a=1;
+      for(int j=5;j>=i;j--)
+      {  
+        System.out.print(a);
+        a++;
+      }
+      System.out.println();
+    }
+}
+}
